f8323e8edb1e4d3392f2a94afb679f92d690e93f
游小爸 <joetww@users.noreply.github.com>
2024-09-10 15:24:26 +0800

    Update checkCaCerts.sh

--- checkCaCerts.sh
+++ checkCaCerts.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#尋找可用keytool
-latest_keytool=$(find / -type f -name keytool 2>/dev/null | xargs ls -lt 2>/dev/null | head -n 1 | awk '{print $NF}')
-
 # 優先從 PATH 中尋找 keytool
 if command -v keytool &>/dev/null; then
     # 如果在 PATH 中找到 keytool，使用它
@@ -22,9 +19,12 @@ if [ -z "$cacerts_paths" ]; then
   exit 1
 fi
 
+# 初始化變數，用來追蹤是否所有檔案都包含指定的證書資訊
+all_certs_found=true
+
 # 檢查每個找到的 cacerts 檔案
 for cacerts_path in $cacerts_paths; do
-  echo "找到 cacerts 檔案: $cacerts_path"
+  echo "正在檢查 cacerts 檔案: $cacerts_path"
 
   # 獲取證書資訊
   cert_info=$($keytool_path -list -v -storepass 'changeit' -keystore "$cacerts_path" 2>/dev/null | grep -iE 'O=Google Trust Services LLC|O=Amazon|O=Sectigo Limited')
@@ -34,10 +34,17 @@ for cacerts_path in $cacerts_paths; do
      [[ "$cert_info" == *"O=Amazon"* ]] && \
      [[ "$cert_info" == *"O=Sectigo Limited"* ]]; then
     echo "在檔案 $cacerts_path 中找到所有指定的證書資訊"
-    exit 0
+  else
+    echo "檔案 $cacerts_path 中未找到所有指定的證書資訊"
+    all_certs_found=false
   fi
 done
 
-# 如果沒有找到所有指定的證書資訊
-echo false
-exit 1
+# 根據所有檔案的結果來決定是否成功
+if [ "$all_certs_found" = true ]; then
+  echo "所有 cacerts 檔案中都包含指定的證書資訊"
+  exit 0
+else
+  echo "並非所有 cacerts 檔案都包含指定的證書資訊"
+  exit 1
+fi
